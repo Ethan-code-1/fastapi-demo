fe524b2fa5351493abb7e0e384ab3ed6b957aa2e
Ethan-code-1 O <ebo4dq@virginia.edu>
2024-04-02 00:21:21 +0000

    Completed data project 1 handout

--- app/main.py
+++ app/main.py
@@ -6,8 +6,20 @@ from pydantic import BaseModel
 import json
 import requests
 import boto3
+import os
+import MySQLdb
+from fastapi.staticfiles import StaticFiles
+
+
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static", html=True), name="static")
+
+DBHOST = os.environ.get('DBHOST')
+DBUSER = os.environ.get('DBUSER')
+DBPASS = os.environ.get('DBPASS')
+DB = "ebo4dq"  # replace with your UVA computing ID / database name
 
 # The URL for this API has a /docs endpoint that lets you see and test
 # your various endpoints/methods.
@@ -19,6 +31,24 @@ app = FastAPI()
 def zone_apex():
     return {"Hello": "Hello Anisha"}
         
+@app.get("/albums")
+def get_albums():
+    db = MySQLdb.connect(host=DBHOST, user=DBUSER, passwd=DBPASS, db=DB)
+    c = db.cursor(MySQLdb.cursors.DictCursor)
+    c.execute("""SELECT * FROM albums ORDER BY name""")
+    results = c.fetchall()
+    db.close()
+    return results
+
+@app.get("/albums/{id}")
+def get_one_album(id):
+    db = MySQLdb.connect(host=DBHOST, user=DBUSER, passwd=DBPASS, db=DB)
+    c = db.cursor(MySQLdb.cursors.DictCursor)
+    c.execute("SELECT * FROM albums WHERE id=" + id)
+    results = c.fetchall()
+    db.close()
+    return results
+
 # api calls within an api!
 @app.get("/github/repos/{user}")
 def github_user_repos(user):
